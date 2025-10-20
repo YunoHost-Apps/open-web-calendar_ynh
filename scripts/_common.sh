7646519c8595ef8bc692539a6db6df8d8dc3fa2a
Didier Chavaroche <didier@chavaroche-si.fr>
2025-10-20 19:34:40 +0200

    chagement de common.js

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,3 +3,9 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
+
+change_common_js() {
+    commonfile=$(find $install_dir -name common.js)
+
+    sed -i "s:document.location.host:document.location.host + \"$path\":g" $commonfile
+}
