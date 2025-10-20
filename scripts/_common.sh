8619e886b2d3d726eb770051eb35451086e7e1c6
Didier Chavaroche <didier@chavaroche-si.fr>
2025-10-20 20:43:07 +0200

    deuxième essau

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,12 +4,14 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-change_common_js() {
+reset_common_js() {
     commonfile=$(find $install_dir -name common.js)
 
-    if [[ $1 == true ]]; then
-        sed -i "s:document\.location\.host.*;:document\.location\.host;:g" "$commonfile"
-    fi
+    sed -i "s:document\.location\.host.*;:document\.location\.host;:g" "$commonfile"
+}
+
+change_common_js() {
+    commonfile=$(find $install_dir -name common.js)
 
     sed -i "s:document\.location\.host:document\.location\.host + \"$path\":g" $commonfile
 }
