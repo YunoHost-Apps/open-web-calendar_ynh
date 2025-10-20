52f54ebb1065396838d0f36900dfd3216cdd42de
Didier Chavaroche <didier@chavaroche-si.fr>
2025-10-20 20:35:11 +0200

    essai de common file

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,5 +7,10 @@
 change_common_js() {
     commonfile=$(find $install_dir -name common.js)
 
-    sed -i "s:document.location.host:document.location.host + \"$path\":g" $commonfile
+    if [[ $1 == true ]]; then
+        sed -i "s:document\.location\.host.*;:document\.location\.host;:g" "$commonfile"
+    fi
+
+    sed -i "s:document\.location\.host:document\.location\.host + \"$path\":g" $commonfile
 }
+
